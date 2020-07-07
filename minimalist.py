c79fb8904a2198b39853a40523c566a1d93a5a6f
Joven Soh <SohWeiKiat@gmail.com>
2020-07-07 21:05:34 +0800

    Added my files in

---
+++ minimalist.py
@@ -0,0 +1,124 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import division, print_function, absolute_import
+
+from timeit import time
+import warnings
+import cv2
+import numpy as np
+from PIL import Image
+from yolo import YOLO
+
+from deep_sort import preprocessing
+from deep_sort import nn_matching
+from deep_sort.detection import Detection
+from deep_sort.tracker import Tracker
+from tools import generate_detections as gdet
+import imutils.video
+
+warnings.filterwarnings('ignore')
+
+def main(yolo):
+
+    # Definition of the parameters
+    max_cosine_distance = 0.3
+    nn_budget = None
+    nms_max_overlap = 1.0
+    
+    # Deep SORT
+    model_filename = 'model_data/mars-small128.pb'
+    encoder = gdet.create_box_encoder(model_filename,batch_size=1)
+    
+    metric = nn_matching.NearestNeighborDistanceMetric("cosine", max_cosine_distance, nn_budget)
+    tracker = Tracker(metric)
+
+    writeVideo_flag = True
+    asyncVideo_flag = False
+
+    #Choose video file here
+    file_path = 'sample_input.mp4'
+    if asyncVideo_flag :
+        video_capture = VideoCaptureAsync(file_path)
+    else:
+        video_capture = cv2.VideoCapture(file_path)
+
+    if asyncVideo_flag:
+        video_capture.start()
+
+    if writeVideo_flag:
+        if asyncVideo_flag:
+            w = int(video_capture.cap.get(3))
+            h = int(video_capture.cap.get(4))
+        else:
+            w = int(video_capture.get(3))
+            h = int(video_capture.get(4))
+        fourcc = cv2.VideoWriter_fourcc(*'DIVX')
+        out = cv2.VideoWriter('Apr1_simplified.avi', fourcc, 30, (w, h))
+        frame_index = -1
+
+    fps = 0.0
+    fps_imutils = imutils.video.FPS().start()
+
+    while True:
+        ret, frame = video_capture.read()  # frame shape 640*480*3
+        if ret != True:
+             break
+
+        t1 = time.time()
+
+        image = Image.fromarray(frame[...,::-1])  # bgr to rgb
+        boxs = yolo.detect_image(image)[0]
+        confidence = yolo.detect_image(image)[1]
+
+        features = encoder(frame,boxs)
+
+        detections = [Detection(bbox, confidence, feature) for bbox, confidence, feature in zip(boxs, confidence, features)]
+        
+        # Run non-maxima suppression.
+        boxes = np.array([d.tlwh for d in detections])
+        scores = np.array([d.confidence for d in detections])
+        indices = preprocessing.non_max_suppression(boxes, nms_max_overlap, scores)
+        detections = [detections[i] for i in indices]
+        
+        # Call the tracker
+        tracker.predict()
+        tracker.update(detections)
+
+        frame[frame > 0] = 0
+        for track in tracker.tracks:
+            if not track.is_confirmed() or track.time_since_update > 1:
+                continue 
+            bbox = track.to_tlbr()
+            cv2.putText(frame, str(track.track_id),(int(bbox[0]), int(bbox[1])),0, 5e-3 * 200, (0,255,0),2)
+         
+
+        if writeVideo_flag: # and not asyncVideo_flag:
+            # save a frame
+            out.write(frame)
+            frame_index = frame_index + 1
+
+        fps_imutils.update()
+
+        fps = (fps + (1./(time.time()-t1))) / 2
+        print("FPS = %f"%(fps))
+        
+        # Press Q to stop!
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    fps_imutils.stop()
+    print('imutils FPS: {}'.format(fps_imutils.fps()))
+
+    if asyncVideo_flag:
+        video_capture.stop()
+    else:
+        video_capture.release()
+
+    if writeVideo_flag:
+        out.release()
+
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main(YOLO())
